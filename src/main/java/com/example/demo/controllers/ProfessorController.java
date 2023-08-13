f297cf54613eee5a925fb66c3b14135f2783a506
Douglas08Santos <santosdouglas0808@gmail.com>
2023-08-13 15:47:33 -0300

    show static professores using ModelAndView

--- src/main/java/com/example/demo/controllers/ProfessorController.java
+++ src/main/java/com/example/demo/controllers/ProfessorController.java
@@ -1,13 +1,33 @@
 package com.example.demo.controllers;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.models.Professor;
+import com.example.demo.models.Status;
 
 @Controller
 public class ProfessorController {
     
     @GetMapping("/professores")
-    public String index() {
-        return "professor/index";
+    public ModelAndView index() {
+        Professor professor1 = new Professor("Nome 1",  new BigDecimal(5000), Status.ATIVO);
+        Professor professor2 = new Professor("Nome 2",  new BigDecimal(10000), Status.APOSENTADO);
+        Professor professor3 = new Professor("Nome 3",  new BigDecimal(7500), Status.INATIVO);
+        Professor professor4 = new Professor("Nome 4",  new BigDecimal(9500), Status.AFASTADO);
+        
+        List<Professor> professores = Arrays.asList(professor1, professor2, professor3, professor4);
+
+        // viewName = endereço da pagina template
+        ModelAndView mv = new ModelAndView("professor/index");
+        
+        mv.addObject("professores", professores);
+        return mv;
     }
 }
--- src/main/java/com/example/demo/models/Professor.java
+++ src/main/java/com/example/demo/models/Professor.java
@@ -2,6 +2,8 @@ package com.example.demo.models;
 
 import java.math.BigDecimal;
 
+import io.micrometer.common.lang.NonNull;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -9,23 +11,43 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
+// O lombok getter e setter não funciona com o thymeleaf - triste :[
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class Professor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private BigDecimal salary;
-    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)    
     private Status status;
+
+    public Professor(String name, BigDecimal salary, Status status) {
+        this.name = name;
+        this.salary = salary;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
 }
